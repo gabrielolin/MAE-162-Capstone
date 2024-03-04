7b90af67caf5857b2d58f3fb10d18698b31453f1
gabrielolin <gabrielolin@g.ucla.edu>
2024-03-04 15:48:39 -0800

    test

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from utils import CvFpsCalc
 from model import KeyPointClassifier
 from model import PointHistoryClassifier
 
-
+#test commemt
 def get_args():
     parser = argparse.ArgumentParser()
 
